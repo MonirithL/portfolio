42673b2a0df5e1819a88867c2e1b48e571936e62
MonirithL <lengmonirith@gmail.com>
2026-04-29 20:37:51 +0700

    add rr ui

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<div class="main">
+    <h1>Get ricke rolled!</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=7fmxSJIm1CQU2rOc?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+
+<style>
+    :global(html, body){
+        width: 100%;
+        height: 100%;
+    }
+    *{
+        padding: 0px;
+        margin: 0px;
+        box-sizing: border-box;
+    }
+    :global(body){
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        min-height: 100%;
+    }
+    .main{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        align-items: center;
+    }
+</style>
