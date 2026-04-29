cb828d8b4036b49bc81f758691c4a66004b04ed9
MonirithL <lengmonirith@gmail.com>
2026-04-29 21:20:39 +0700

    fmt2

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	fs := http.FileServer(http.Dir("./ui/build"))
 	http.Handle("/", fs)
-	http.ListenAndServe(":8080", nil)
 	fmt.Println("Starting server on 8080")
+	http.ListenAndServe("localhost:8080", nil)
 }
